cb95f0f7928ef94e546f8eac5c8665a54fe40a4a
Emilgon <g.emilio@correo.unimet.edu.ve>
2023-03-11 03:10:31 -0400

    Landing de lapágina, muestra la api de las peliculas
    falta arreglar para que muestre info

---
+++ src/utils/httpClient.js
@@ -0,0 +1,11 @@
+const API = "https://api.themoviedb.org/3";
+
+export function get(path) {
+  return fetch(API + path, {
+    headers: {
+      Authorization:
+        "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2MjJmODc3ZmEzOTk2NDI4ZGU4MWQzODdmOTIzMjllMCIsInN1YiI6IjY0MGMwN2JjZmJhNjI1MDEwYWE5ZDIxNyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.bs5L_1_ww6vyotAay__QWhlQKtgOLDVawmFsRn0RH2Q",
+        "Content-Type": "application/json;charset=utf-8",
+    },
+  }).then((result) => result.json());
+}
