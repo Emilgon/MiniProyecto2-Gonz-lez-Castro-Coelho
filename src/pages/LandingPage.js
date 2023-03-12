586914c928d47a8ecbf8da7d4f1977318fc58807
Gabriel <gabriel.coelho@correo.unimet.edu.ve>
2023-03-12 13:50:33 -0400

    Detalles

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,12 +1,6 @@
 import { MoviesGrid } from "../components/MoviesGrid";
 import { Search } from "../components/Search";
 import "./EnterPage.css";
-import { createRoot } from "react-dom/client";
-import { EnterPage } from "./EnterPage";
-
-
-const rootElement = document.getElementById("root");
-const root = createRoot(rootElement);
 
 function Redirect(){
   window.location.reload(false);
