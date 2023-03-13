62c2f9f12f237e1057c490365cb1aff7920cce78
Emilgon <g.emilio@correo.unimet.edu.ve>
2023-03-12 22:13:32 -0400

    final final

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -2,12 +2,13 @@ import { MoviesGrid } from "../components/MoviesGrid";
 import { Search } from "../components/Search";
 import "./EnterPage.css";
 import { useQuery } from "../hooks/useQuery";
-
+import { Redirect } from "react-router-dom";
 export function LandingPage() {
   const query = useQuery();
   const search = query.get("search");
   return (
     <div>
+      <button onClick={Redirect} className="logout-btn">LOG OUT</button>
       <Search />
       <MoviesGrid key={search} search={search}/>
     </div>
